35ad5b3e0075fbf6b428c9a87d127d0050e7f66a
miyoku157 <miyoku157@gmail.com>
2017-01-26 11:43:16 +0100

    crosshair

---
+++ Roid Ring/Assets/Aim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+    Texture2D crosshair;
+    Rect position;
+	// Use this for initialization
+	void Start () {
+        crosshair=Resources.Load<Texture2D>("eotech_a65_popup");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position = new Rect(Input.mousePosition.x-(crosshair.width/2), -(Input.mousePosition.y- (crosshair.height / 2)), crosshair.width, crosshair.height);
+	}
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(position, crosshair);
+    }
+}
