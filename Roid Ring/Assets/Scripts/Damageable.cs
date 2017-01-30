9c1372e3688c6595a3912d888119d091c2ab4ad9
Exospector <pcheminel@gmail.com>
2017-01-30 17:47:48 +0100

    Add shield code + Add Sun

--- Roid Ring/Assets/Scripts/Damageable.cs
+++ Roid Ring/Assets/Scripts/Damageable.cs
@@ -1,33 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Damageable : MonoBehaviour
-{
-    public int health;
-    public bool isPlayer;
-
-    public void TakeDamage(int amount)
-    {
-        health -= amount;
-
-        if(health < 0)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-    void OnCollisionEnter(Collision coll)
-    {
-        if(coll.gameObject.tag == "Shot")
-        {
-            TakeDamage(10);
-            Destroy(coll.gameObject);
-        }
-    }
-
-    public int getHealth()
-    {
-        return health;
-    }
-}
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class Damageable : MonoBehaviour {     public int maxHull, maxShield;     public float shieldRechargeRate = 1;     public bool isPlayer;      private int hull, shield;     private int temp;      void Start()     {         shield = maxShield;         hull = maxHull;          if(maxShield > 0)         {             StartCoroutine("ShieldRecharge");         }     }      public void TakeDamage(int amount)     {         temp = 0;         shield -= amount;                      if(shield < 0)         {             shield = 0;             temp = -shield;             hull -= temp;         }          if(hull < 0)         {             Destroy(this.gameObject);         }     }      void OnCollisionEnter(Collision coll)     {         if(coll.gameObject.tag == "Shot")         {             TakeDamage(10);             Destroy(coll.gameObject);         }     }      IEnumerator ShieldRecharge()     {         while (true)         {             if (shield < maxShield)             {                 shield += 1;             }              yield return new WaitForSeconds(1 / shieldRechargeRate / 6.66f);         }     }      public int getHull()     {         return hull;     }      public int getShield()     {         return shield;     } }
