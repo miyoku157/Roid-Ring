bdc16c3824466aeb5d937283cf024bee23f369b1
Exospector <pcheminel@gmail.com>
2017-01-30 15:28:07 +0100

    Update WeaponController, Aim, playerPrefab and Main Scene

--- Roid Ring/Assets/Scripts/Aim.cs
+++ Roid Ring/Assets/Scripts/Aim.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Aim : MonoBehaviour {
-    Texture2D crosshair;
+public class Aim : MonoBehaviour
+{
+    public Texture2D crosshair;
     Rect position;
-	// Use this for initialization
-	void Start () {
+    
+	void Start ()
+    {
         Cursor.visible = false;
-        crosshair=Resources.Load<Texture2D>("eotech_a65_popup");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        position = new Rect(Input.mousePosition.x-(crosshair.width/2), -(Input.mousePosition.y- crosshair.height), crosshair.width, crosshair.height);
+	void Update ()
+    {
+        position = new Rect(Input.mousePosition.x - (crosshair.width / 2), -(Input.mousePosition.y - crosshair.height), crosshair.width, crosshair.height);
 	}
 
     private void OnGUI()
